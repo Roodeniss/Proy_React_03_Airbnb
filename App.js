c4a1a1cc224ac58b4d113193710040ba0c5f4a54
Roodeniss <66141538+Roodeniss@users.noreply.github.com>
2023-01-07 15:16:14 -0300

    Hero secction

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Navbar from "./componets/Nabar"
+import Hero from "./componets/Hero"
+
 
 export default function App() {
     return (
        <div>
+
         <Navbar/>
+        <Hero/>
        </div>
     )
 }
