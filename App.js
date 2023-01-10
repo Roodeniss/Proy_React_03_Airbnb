499e639cd7d5c86b862ec0a153d2b7ac81b0cad0
Roodeniss <66141538+Roodeniss@users.noreply.github.com>
2023-01-10 10:57:44 -0300

    cards css

--- App.js
+++ App.js
@@ -22,14 +22,9 @@ export default function App() {
 
             <Navbar />
             <Hero />
-            <Card
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            <section className=" cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
