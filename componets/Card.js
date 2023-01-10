a71c1f775c5d4228c4ee9f1825c54c46262a0fab
Roodeniss <66141538+Roodeniss@users.noreply.github.com>
2023-01-10 12:43:11 -0300

    update

--- componets/Card.js
+++ componets/Card.js
@@ -11,15 +11,16 @@ export default function Card(props) {
     return (
         <div className="card">
             {badgeText && <div className="card-badge">{badgeText}</div>}
-            <img src={`../images/${props.img}`} className="card-image" />
+            <img src={`../images/${props.coverImg}`} className="card-image" />
             <div className="card-stats">
                 <img src="../images/star.png" className="card-star" />
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.country}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card-title">{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / 
+            person</p>
         </div>
     )
 }
